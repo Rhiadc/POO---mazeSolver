e6b2ae665fe306314654ea571490277d71435d3b
Rhiad Ciccoli <who.4004@gmail.com>
2018-07-31 15:42:39 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -7,15 +7,16 @@ public class Principal{
 		Caminho saida = new Saida();
 		Caminho asterisco = new Asterisco();
 
-		labirinto.soluciona();
 
 		Object[][] objeto = new Object[13][13];
-		objeto = labirinto.insere((Livre) livre, (Parede) parede, (Saida) saida, (Entrada) entrada);
-		//labirinto.imprime();
+		char[][] mapaConvertido = new char[13][13];
+		mapaConvertido = labirinto.soluciona();
+
+		objeto = labirinto.criaMatrizCaminho(mapaConvertido, (Livre) livre, (Parede) parede, (Saida) saida, (Entrada) entrada);
+		int coordenada = labirinto.getCoordenadaEntrada();
 
 		MazeSolver mazeSolver = new MazeSolver(objeto, (Parede) parede, (Entrada) entrada, (Livre) livre, (Saida) saida, (Asterisco) asterisco);
-		mazeSolver.mazeRecursive(1, 1, -1);
+		mazeSolver.mazeRecursive(coordenada, 1, -1);
 		mazeSolver.imprime();	
-		//labirinto.imprimeTeste();	
 	}
 }
