03a16d588006dc1f1b2535a88db3acda166f4925
Rhiad Ciccoli <who.4004@gmail.com>
2018-07-31 23:26:26 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,12 +1,23 @@
+import java.util.Scanner;
 public class Principal{
 	public static void main(String[] args){
-		Maze labirinto = new Maze(0);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Entrada para o labirinto (digite um valor de 0 a 3): ");
+		int num1 = sc.nextInt();
+		while(num1 != 0 && num1 != 1 && num1 != 2 && num1 != 3){
+			System.out.print("Erro, favor digite um valor de 0 a 3: ");
+				num1 = sc.nextInt();
+		}
+		long inicio = System.currentTimeMillis(); 
+		
+		Maze labirinto = new Maze(num1);
 		Caminho parede = new Parede();
 		Caminho livre = new Livre();
 		Caminho entrada = new Entrada();
 		Caminho saida = new Saida();
 		Caminho asterisco = new Asterisco();
 
+		
 
 		Object[][] objeto = new Object[13][13];
 		char[][] mapaConvertido = new char[13][13];
@@ -17,6 +28,8 @@ public class Principal{
 
 		MazeSolver mazeSolver = new MazeSolver(objeto, (Parede) parede, (Entrada) entrada, (Livre) livre, (Saida) saida, (Asterisco) asterisco);
 		mazeSolver.mazeRecursive(coordenada, 1, -1);
-		mazeSolver.imprime();	
+		mazeSolver.imprime();
+		long fim  = System.currentTimeMillis();
+		System.out.println("Tempo gasto: " +  (fim - inicio) + "ms");	
 	}
 }
